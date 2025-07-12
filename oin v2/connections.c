e6c3c1b9ff8b3f2908c3cb5b1e39254a7324ca98
Ilyaa023 <96044968+Ilyaa023@users.noreply.github.com>
2025-07-13 00:39:07 +0500

    bmn code

---
+++ oin v2/connections.c
@@ -0,0 +1,113 @@
+#include "main.h"
+
+void CLK_Init(){
+	//fCPU = 16 MHz
+	CLK_CKDIVR_HSIDIV = 0;
+	CLK_CKDIVR_CPUDIV = 0;
+	// Enable HSE
+	/*CLK_SWR = 0xB4;*/
+	CLK_PCKENR1 = 0xFF;
+	CLK_PCKENR2 = 0xFF;
+}
+
+void UART1_Init(){
+	UART1_CR1_UART0 = 1;  // set UARTD, UART1 disable
+	// 115200 Baud Rate, when fMASTER=16MHz
+	UART1_BRR2=0x0b;
+	UART1_BRR1=0x08;
+	// 1000000 Baud Rate, when fMASTER=16MHz
+//	UART1_BRR2=0x00;
+//	UART1_BRR1=0x01;
+	UART1_CR2_TEN = 1;    // set TEN, Transmission Enable
+	UART1_CR2_REN = 1;    // set REN, Recieve Enable
+//	UART1_CR2_RWU = 1;		// set RWU, Receiver wakeup
+	UART1_CR1_UART0 = 0;  // clear UARTD, UART1 enable
+}
+
+void GPIO_Init(){
+	PA_DDR_DDR1 = 1;
+	PA_DDR_DDR2 = 1;
+	PA_DDR_DDR3 = 1;
+	PB_DDR_DDR4 = 1;
+	PC_DDR_DDR3 = 1;
+	PC_DDR_DDR4 = 1;
+	PC_DDR_DDR5 = 1;
+	PC_DDR_DDR6 = 1;
+	PC_DDR_DDR7 = 1;
+	PD_DDR_DDR2 = 1;
+	PD_DDR_DDR3 = 1;
+	PD_DDR_DDR4 = 1;
+
+	PA_CR1_C11 = 1;
+	PA_CR1_C12 = 1;
+	PA_CR1_C13 = 1;
+	PB_CR1_C14 = 1;
+	PC_CR1_C13 = 1;
+	PC_CR1_C14 = 1;
+	PC_CR1_C15 = 1;
+	PC_CR1_C16 = 1;
+	PC_CR1_C17 = 1;
+	PD_CR1_C12 = 1;
+	PD_CR1_C13 = 1;
+	PD_CR1_C14 = 1;
+
+	PA_CR2_C21 = 0;
+	PA_CR2_C22 = 0;
+	PA_CR2_C23 = 0;
+	PB_CR2_C24 = 0;
+	PC_CR2_C23 = 0;
+	PC_CR2_C24 = 0;
+	PC_CR2_C25 = 0;
+	PC_CR2_C26 = 0;
+	PC_CR2_C27 = 0;
+	PD_CR2_C22 = 0;
+	PD_CR2_C23 = 0;
+	PD_CR2_C24 = 0;
+
+	PA_ODR_ODR1 = 1;
+	PA_ODR_ODR2 = 1;
+	PA_ODR_ODR3 = 1;
+	PB_ODR_ODR4 = 1;
+	PC_ODR_ODR3 = 1;
+	PC_ODR_ODR4 = 1;
+	PC_ODR_ODR5 = 1;
+	PC_ODR_ODR6 = 1;
+	PC_ODR_ODR7 = 1;
+	PD_ODR_ODR2 = 1;
+	PD_ODR_ODR3 = 1;
+	PD_ODR_ODR4 = 1;
+}
+
+void DecomposeCom (unsigned char data){
+	if (data>>5 == OIN_DISABLE>>5){
+		if ((data & COM_POWER_MASK) == (255 & COM_POWER_MASK))
+			SET_POWER;
+		else
+			SET_GROUND;
+
+		if ((data & COM_BALANCE_MASK) == (255 & COM_BALANCE_MASK))
+			SET_BALANCE
+		else if ((data & COM_OIN_PLUS1_MASK) == (255 & COM_OIN_PLUS1_MASK))
+			SET_PLUS1
+		else if((data & COM_OIN_PLUS2_MASK) == (255 & COM_OIN_PLUS2_MASK))
+			SET_PLUS2
+		else
+			RESET_PLUS
+
+		if ((data & COM_COMPORATOR_MASK) == (255 & COM_COMPORATOR_MASK))
+			SET_COM
+		else if ((data & COM_OIN_MINUS1_MASK) == (255 & COM_OIN_MINUS1_MASK))
+			SET_MINUS1
+		else if((data & COM_OIN_MINUS2_MASK) == (255 & COM_OIN_MINUS2_MASK))
+			SET_MINUS2
+		else
+			RESET_MINUS
+	}
+}
+
+void Ping (){
+//	SET_LED4;
+	SET_GROUND;
+	delay(500000);
+	SET_POWER;
+}
---
+++ oin v2/main.h
@@ -0,0 +1,56 @@
+/*
+A1 -> clkRES (write when LOW)
+A2 -> two LEDs
+A3 -> dataRES
+
+B4 -> balanceControl
+B5 -> NC
+
+C7 -> csRES (LOW to write in resistor via spi, HIGH to set resistance)
+C6 -> comparatorControl
+C5 -> oinMinusControl
+C4 <- oin+1 input
+C3 -> oinPlusControl
+
+D1 -> SWIM
+D2 -> shotdown in/out
+D3 -> powerControl (blue resistor)
+D4 -> single LED
+D5 -> TX
+D6 -> RX
+*/
+
+#include "iostm8s003f3.h"
+#include "oin_global_interface.h"
+#include "legacy.h"
+
+#define SET_POWER       PD_ODR_ODR4 = 0
+#define SET_GROUND      PD_ODR_ODR4 = 1
+#define RESET_PLUS      {PD_ODR_ODR3 = 0; PC_ODR_ODR3 = 1; PD_ODR_ODR2 = 1;}
+#define SET_PLUS1       {PD_ODR_ODR3 = 0; PC_ODR_ODR3 = 1; PD_ODR_ODR2 = 0;}
+#define SET_PLUS2       {PD_ODR_ODR3 = 0; PC_ODR_ODR3 = 0; PD_ODR_ODR2 = 1;}
+#define SET_BALANCE     {PD_ODR_ODR3 = 1; PC_ODR_ODR3 = 1; PD_ODR_ODR2 = 1;}
+#define RESET_MINUS     {PA_ODR_ODR1 = 1; PC_ODR_ODR7 = 1; PC_ODR_ODR4 = 1;}
+#define SET_MINUS1      {PA_ODR_ODR1 = 1; PC_ODR_ODR7 = 0; PC_ODR_ODR4 = 1;}
+#define SET_MINUS2      {PA_ODR_ODR1 = 1; PC_ODR_ODR7 = 1; PC_ODR_ODR4 = 0;}
+#define SET_COM         {PA_ODR_ODR1 = 0; PC_ODR_ODR7 = 1; PC_ODR_ODR4 = 1;}
+
+#define SET_LED4       	PB_ODR_ODR4 = 0
+#define RESET_LED4     	PB_ODR_ODR4 = 1
+
+#define ID 200 			//individual ID minimum 1, maximum 255 (except cases when logging via the same port)
+#define DELAY_TIME_RES 320
+
+static void delay(unsigned long t){ while(t--); }
+
+void CLK_Init();
+void UART1_Init();
+void GPIO_Init();
+void UART1_send_char(char ch);
+void UART1_print_string(char *str);
+void UART1_print_bcd(unsigned char num);
+void SetResistor(unsigned char num, unsigned char value);
+
+void DecomposeCom (unsigned char data);
+void Ping ();
+
